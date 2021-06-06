ae69c4223b682660473822f688900204059060da
anjvlog <85437856+anjvlog@users.noreply.github.com>
2021-06-06 19:33:23 +0530

    Add files via upload

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <h1> new project in REACT</h1>
+    </header>
+  );
+}
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note() {
+  return (
+    <div className="note">
+      <h1> Java and React JS</h1>
+      <p>This was amazing bootcamp.we learn so many new concepts 
+        related to HTML,JS,React</p>
+    </div>
+  );
+}
+export default Note;
